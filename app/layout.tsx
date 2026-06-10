a4cd96856abe26af22fa78886cc78f4e42a82013
navaneethanlr <navaneethanlr@gmail.com>
2026-06-10 14:25:06 +0530

    update icon URLs and add new portfolio icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,15 +22,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/icons8-portfolio-32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/icons8-portfolio-32.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/icons8-portfolio-32.png',
         type: 'image/svg+xml',
       },
     ],
